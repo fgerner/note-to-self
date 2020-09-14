b87470f45000bf73896cd5add8b9311ed9d17ff5
Fred <fred.gerner@icloud.com>
2020-09-14 22:20:52 +1000

    added cookie functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import {Button, Form, FormControl} from "react-bootstrap";
+import Note from "./Note";
+import {bake_cookie, read_cookie} from 'sfcookies';
+
+const cookie_key = 'NOTES';
 
 class App extends Component {
     constructor(props) {
@@ -10,26 +14,30 @@ class App extends Component {
         };
     }
 
+    componentDidMount() {
+        this.setState({notes: read_cookie(cookie_key)});
+    }
+
     submit() {
-        const notes = this.state.notes;
-        const newNote = {text: this.state.text};
-        notes.push(newNote);
-        this.setState({notes: notes});
+        const {notes, text} = this.state;
+        notes.push({ text});
+        this.setState({ notes});
+        bake_cookie(cookie_key, this.state.notes)
     }
 
     render() {
         return (
             <div>
                 <h2>Note to self</h2>
-                <Form inline>
-                    <FormControl onChange={event => this.setState({text: event.target.value})} />
+                <Form inline={true}>
+                    <FormControl onChange={event => this.setState({text: event.target.value})}/>
                     {' '}
                     <Button onClick={() => this.submit()}>Submit</Button>
                 </Form>
                 {
-                    this.state.notes.map(note => {
+                    this.state.notes.map((note, index) => {
                         return (
-                            <div>{note.text}</div>
+                            <Note key={index} note = { note }/>
                         )
                     })
                 }
