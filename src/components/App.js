0d78ca914a12baf574cf22b6e6cbffc32a392d69
Fred <fred.gerner@icloud.com>
2020-09-14 20:01:21 +1000

    wip: added form and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,40 @@
 import React, {Component} from 'react';
+import {Button, Form, FormControl} from "react-bootstrap";
 
 class App extends Component {
-    render(){
-        return (<div><h2>Note to self</h2></div>)
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+            notes: []
+        };
+    }
+
+    submit() {
+        const notes = this.state.notes;
+        const newNote = {text: this.state.text};
+        notes.push(newNote);
+        this.setState({notes: notes});
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Note to self</h2>
+                <Form inline>
+                    <FormControl onChange={event => this.setState({text: event.target.value})} />
+                    {' '}
+                    <Button onClick={() => this.submit()}>Submit</Button>
+                </Form>
+                {
+                    this.state.notes.map(note => {
+                        return (
+                            <div>{note.text}</div>
+                        )
+                    })
+                }
+            </div>
+        )
     }
 }
 
